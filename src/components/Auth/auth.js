7ac0bc2858c7df64399c402d952fe1261e33d3e0
Wayne Collier <ke4tri@gmail.com>
2018-11-17 11:49:21 -0600

    cards printing to dom

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -5,6 +5,25 @@ import getTasks from '../../helpers/dataGetter';
 import './auth.scss';
 import checkLoginStatus from '../../helpers/authHelpers';
 
+
+const printTask = (dataArray) => {
+  let domString = '';
+  dataArray.forEach((data) => {
+    domString += `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Task number ${data.id}</h5>
+      <p class="card-text">Task : ${data.task}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    `;
+  });
+  $('#taskPrint').append(domString);
+};
+
+
 const shootTask = () => {
   const domString = `
   <button id="taskBut" class="btn btn-secondary mt-5">
@@ -13,7 +32,7 @@ const shootTask = () => {
   $('#taskBut').on('click', () => {
     getTasks()
       .then((data) => {
-        displayTasks(data);
+        printTask(data);
       }).catch((error) => {
         console.error(error);
       });
